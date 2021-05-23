1f279e23cf48a0058df811d126bc8b829f66c65f
Mevzin <thiagomev@gmail.com>
2021-05-23 19:47:37 -0300

    Filter Activation

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,12 +8,26 @@ import { CourseService } from "./course.service";
 })
 export class CourseListComponent implements OnInit {
 
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy: string;
 
   constructor(private couseService: CourseService){}
 
   ngOnInit(): void{
-    this.courses = this.couseService.retriveAll();
+    this._courses = this.couseService.retriveAll();
+    this.filteredCourses = this._courses;
   }
 
+  set filter(value: string){
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+
+  get filter(){
+    return this._filterBy;
+  }
 }
